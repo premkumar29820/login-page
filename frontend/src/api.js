fc355e6e6b5ba602d0e5125eafeda034daffd537
PREMKUMAR <167052615+premkumar29820@users.noreply.github.com>
2026-09-05 19:41:37 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,17 +1,13 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL:"https://login-page-obqq.onrender.com",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-export const loginUser = (email, password) => {
-  return api.post("/login", {
-    email,
-    password,
-  });
-};
-
-export default api;
+const response = await fetch(
+  `${import.meta.env.VITE_API_URL}/api/login`,
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  }
+);
